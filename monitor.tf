8513469ff7c2612bf407acde23dbbc92d30136b5
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-18 08:53:45 +0530

    Update terraform-modules/terraform-tags/

--- monitor.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_ec2_tag" "monitor-tag" {
-  count       = var.RESOURCE_ID_COUNT
-  resource_id = element(var.ALL_TAG_IDS, count.index)
-  key         = "Monitor"
-  value       = "yes"
-}
